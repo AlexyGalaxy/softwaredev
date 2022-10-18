9f4fdf00fc034eb194f6453b2e7276b6f5ec9f29
Alex <alex@viscofamily.com>
2022-10-18 15:54:10 -0400

    Only Works with Compiler jdk-13

--- JavaFxTest.java
+++ JavaFxTest.java
@@ -4,23 +4,16 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -35,51 +28,85 @@ import java.util.TreeSet;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
 
 import application.JavaFxTest.Word;
 
 public class JavaFxTest extends Application {
-	private static Map<String, String> gsMap = new HashMap<String, String>();
+	private static ObservableList<String> wordPlace = FXCollections.observableArrayList();
+	private static ObservableList<String> wordWord = FXCollections.observableArrayList();
 	
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane p = new AnchorPane();
 			//AnchorPane p = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene1 = new Scene(p,600,500);
+			Scene scene1 = new Scene(p,200,610);
+			
 			Text textI = new Text();
 			ListView<String> textO = new ListView<String>();
-			//TextField textO = new TextField();
-			//textO.setId(getMessages());
+			ListView<String> textO1 = new ListView<String>();
+			ListView<Integer> counter = new ListView<Integer>();
 		    
 			textI.setText("Press Calculate To Display \nThe 20 Most Words Used");
-			textI.setLayoutX(235);
+			textI.setLayoutX(35);
 			textI.setLayoutY(20);
 			
 			Button b1 = new Button();
 			b1.setOnAction(e -> primaryStage.setScene(scene1)); 
 			b1.setText("Calculate!");
-			b1.setLayoutX(270);
+			b1.setLayoutX(70);
 			b1.setLayoutY(50);
 			
-			textO.setLayoutX(185);//output
+			counter.setLayoutX(15);//counter output
+			counter.setLayoutY(90);
+			counter.setMaxWidth(100);
+			counter.setMinHeight(480);
+						
+			textO.setLayoutX(50);//word output
 			textO.setLayoutY(90);
+			textO.setMaxWidth(100);
+			textO.setMinHeight(480);
 			
+			textO1.setLayoutX(125);//# used output
+			textO1.setLayoutY(90);
+			textO1.setMaxWidth(60);
+			textO1.setMinHeight(480);
 			
+			Button b2 = new Button();
+			
+			b2.setOnAction(e -> primaryStage.setScene(scene1)); //what do when press b1
+			b2.setText("Clear All");
+			b2.setLayoutX(70);
+			b2.setLayoutY(575);
+		
 			b1.setOnAction(new EventHandler<ActionEvent>() {  
-				  
 				public void handle(ActionEvent arg0) {
 					textO.getItems().clear();
-					textO.getItems().add("The 20 most used words:");
-			        textO.getItems().add("~~~~~~~~~~~~~~~~~");
-			        textO.getItems().add(gsMap.get("")); //display top 20
-				}  
+					textO1.getItems().clear();
+					counter.getItems().clear();
+					textO.getItems().addAll(wordWord);
+					textO1.getItems().addAll(wordPlace);
+					for(int i=1; i<21; i++) {
+						counter.getItems().addAll(i);
+					}
+	 			}  
 			} );
-					
+			
+			b2.setOnAction(new EventHandler<ActionEvent>() {  //what do when press b2
+				public void handle(ActionEvent arg0) {
+					textO.getItems().clear();
+					textO1.getItems().clear();
+					counter.getItems().clear();
+				}
+			});
+			
 			p.getChildren().add(b1);
 			p.getChildren().add(textI);
+			p.getChildren().add(counter);
 			p.getChildren().add(textO);
+			p.getChildren().add(textO1);
+			p.getChildren().add(b2);
 			
-			//scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene1);
 			primaryStage.setTitle("UI Design Assignment");
 			primaryStage.show();
@@ -93,7 +120,7 @@ public class JavaFxTest extends Application {
 	public static void main(String[] args) throws IOException {
 		launch(args); }
 	
-	public static Collection<Word> wordFind() throws IOException{
+	public Collection<Word> wordFind() throws IOException{
 		Map<String, Word> map = new HashMap<String, Word>();
 		Document doc = Jsoup.connect("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm").get();
 
@@ -125,18 +152,17 @@ public class JavaFxTest extends Application {
         int i = 0;
         int maxWords = 20;
         
-        
-		for (Word currentWord : sortedWords) { //not working
+		for (Word currentWord : sortedWords) { 
 			
             if (i >= maxWords) { 
             	break;
             }
-   
             String numberUsed=String.valueOf(currentWord.count);//convert num to string
             String listWord=String.valueOf(currentWord.word); // convert 'Map<String,JavaFxTest.Word>' to string
-            gsMap.put(numberUsed, listWord);//store values into private hash map gsMap
-           
-            System.out.println(currentWord.word + "\t" + currentWord.count ); //console print
+
+            wordPlace.add(numberUsed);//store values into private hash map gsMap
+            wordWord.add(listWord);
+
             i++;
 		}
 		return sortedWords;
@@ -157,13 +183,5 @@ public class JavaFxTest extends Application {
 		public int compareTo(Word a) { 
     	   return a.count - count;
 		}
-	}
-
-	public Map<String, String> getMap() {
-       return this.gsMap;
-	}
-
-	public void setMap(HashMap<String, String> input) {
-       this.gsMap = input;
 	}
 }
